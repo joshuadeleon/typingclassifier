be535e7756345764b16a9cc142ecfb44025f685c
Joshua DeLeon <josh@transtatic.com>
2016-09-08 08:40:09 -0700

    Initial commit

---
+++ ML.TypingClassifier/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ML.TypingClassifier.Startup))]
+namespace ML.TypingClassifier
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
