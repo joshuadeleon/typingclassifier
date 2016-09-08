6aaa6ee51f739363b1e74e00c35a6b541742c0ce
Michael Beidler <michael.beidler@gmail.com>
2016-09-08 13:01:48 -0700

    Add some feature extractors. Add sample text.

---
+++ ML.TypingClassifier/Models/MistakeCountExtractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ML.TypingClassifier.Models
+{
+    public class MistakeCountExtractor : IFeatureExtractor
+    {
+        public double Extract(Timeline timeline)
+        {
+            return timeline.Events.Where(e => e.Key == "Backspace").Count();
+        }
+    }
+}
